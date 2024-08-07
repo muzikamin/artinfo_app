69705f2a6fb5c05d2a4ca471e5362c13b408b493
muzikamin <muzikamin@naver.com>
2024-08-07 18:41:03 +0900

    오류

--- src/api.js
+++ src/api.js
@@ -1,13 +1,17 @@
 const fetch = require("node-fetch");
 
-const BaseUrl = "http://api.kcisa.kr/openapi/API_CCA_145/request?";
+const BaseUrl = "http://api.kcisa.kr/openapi/API_CCA_145/request";
 
 const options = {
   method: "GET",
   headers: {
     accept: "application/json",
-    serviceKey: "dc3f4754-7de7-4dc7-91f3-e5e5387824b7",
+    "Access-Control-Allow-origin": "*",
   },
 };
 
-export const listData = fetch(BaseUrl, options).then((res) => res.json());
+const serviceKey = "?serviceKey=dc3f4754-7de7-4dc7-91f3-e5e5387824b7";
+
+export const listData = fetch(BaseUrl + serviceKey, options).then((res) =>
+  res.json()
+);
